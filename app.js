9e3c7cb47d519095a462f33dcc85658fb9c7a097
Ahmet Kutay Karacair <akkaracair@gmail.com>
2021-08-19 23:40:34 +0300

    added routes,views and public folder, updated app.js and added shop, add-product and pageNotFound html

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const http = require("http");
-const routes = require("./routes");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const pageNotFoundRoutes = require("./routes/pageNotFound");
+const path = require("path");
 
-const server = http.createServer(routes.handler);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+app.use(pageNotFoundRoutes);
+
+const server = http.createServer(app);
 
 server.listen(8000);
